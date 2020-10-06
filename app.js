4c6fc0f02afa2d7fee93cd8e1c3a1d0989e0a271
alisaid-sm <alisaid7170@gmail.com>
2020-10-06 15:34:24 +0700

    first socket.io

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 const express = require('express')
 
+const http = require('http')
+
+// eslint-disable-next-line no-unused-vars
+const socketio = require('socket.io')
+
 const bodyParser = require('body-parser')
 
 const cors = require('cors')
@@ -12,7 +17,20 @@ const path = require('path')
 const ejs = require('ejs')
 
 const app = express()
+const server = http.createServer(app)
+const io = socketio(server)
 
+io.on('connection', (socket) => {
+  console.log('user connected')
+
+  socket.on('send-message', (msg) => {
+    io.emit('get-message', msg)
+  })
+
+  socket.on('disconnect', () => {
+    console.log('user disconnect')
+  })
+})
 // eslint-disable-next-line no-undef
 app.set('views', path.join(__dirname, 'src/views'))
 app.set('view engine', 'ejs')
@@ -29,6 +47,6 @@ app
   .use('/api/v1', usersRouter)
   .use(express.static('src/upload'))
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`server running on port ${PORT}`)
 })
--- src/models/users.js
+++ src/models/users.js
@@ -58,7 +58,7 @@ const users = {
   },
   getDetail: (id) => {
     return new Promise((resolve, reject) => {
-      db.query(`SELECT * FROM users WHERE id='${id}'`, (err, result) => {
+      db.query(`SELECT * FROM users WHERE name='${id}'`, (err, result) => {
         if (err) {
           reject(new Error(err))
         } else {
